081a45d5db19eaa1646be2665fb613d92d3807dc
Samuel Ogboye <ogboyesam@gmail.com>
2023-10-25 00:33:46 +0100

    connect to DB

--- app.py
+++ app.py
@@ -1,15 +1,43 @@
-from flask import Flask, render_template
-from wtform_fields import *
+from flask import Flask, render_template, jsonify, request
+from wtform_fields import RegistrationForm
+from dotenv import load_dotenv
+import os
+from models import db, User
+
+# Load environment variables from .env
+load_dotenv()
 
 
 app = Flask(__name__)
-app.secret_key = 'secret'
+app.secret_key = os.getenv('SECRET_KEY')
+
+# configure database
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+
+# Initialize SQLAlchemy with your Flask app
+db.init_app(app)
+
+# db = SQLAlchemy(app)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     reg_form = RegistrationForm()
     if reg_form.validate_on_submit():
-        return "Great Success!"
+        username = reg_form.username.data
+        password = reg_form.password.data
+
+        # check username exists
+        user_object = User.query.filter_by(username=username).first()
+        if user_object:
+            return "Someone else has taken this username!"
+
+        # Add user to database
+        user = User(username=username, password=password)
+        db.session.add(user)
+        db.session.commit()
+        return "Inserted into DB!"
+
 
     return render_template('index.html', form=reg_form)
 
