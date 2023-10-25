5fa32ed32a0649644e1cf235fdfa5d6ea6698386
Samuel Ogboye <ogboyesam@gmail.com>
2023-10-25 01:01:39 +0100

    create login page and login validation

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, jsonify, request
-from wtform_fields import RegistrationForm
+from flask import Flask, render_template, redirect, url_for
+from wtform_fields import RegistrationForm, LoginForm
 from dotenv import load_dotenv
 import os
 from models import db, User
@@ -21,6 +21,8 @@ db.init_app(app)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     reg_form = RegistrationForm()
+
+    # Update database if validation success
     if reg_form.validate_on_submit():
         username = reg_form.username.data
         password = reg_form.password.data
@@ -29,12 +31,21 @@ def index():
         user = User(username=username, password=password)
         db.session.add(user)
         db.session.commit()
-        return "Inserted into DB!"
+        return redirect(url_for('login'))
 
 
     return render_template('index.html', form=reg_form)
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
 
+    login_form = LoginForm()
+
+    # Allow login if validation success
+    if login_form.validate_on_submit():
+        return "Logged in, finally!"
+
+    return render_template('login.html', form=login_form)
 
 
 if __name__ == '__main__':
